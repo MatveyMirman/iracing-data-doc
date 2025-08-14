f0c1948439ea5591378d95622ce38db836281988
Matvey Mirman <mirman.m@gmail.com>
2025-08-14 09:47:57 +0000

    feat: Add PrismJS for syntax highlighting in CodeBlock component

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,5 +1,8 @@
 "use client";
 import * as React from "react";
+import "prismjs";
+import "prismjs/components/prism-typescript";
+import "prismjs/themes/prism-tomorrow.css";
 
 interface CodeBlockProps {
   code: string;
@@ -8,10 +11,19 @@ interface CodeBlockProps {
 }
 
 export function CodeBlock({ code, language = "typescript", className = "" }: CodeBlockProps) {
+  const ref = React.useRef<HTMLElement>(null);
+
+  React.useEffect(() => {
+    if (ref.current) {
+      // @ts-ignore
+      window.Prism && window.Prism.highlightElement(ref.current);
+    }
+  }, [code, language]);
+
   return (
     <pre className={`p-4 bg-muted rounded text-xs overflow-x-auto max-h-96 font-mono ${className}`}
          style={{ fontFamily: 'Menlo, monospace' }}>
-      <code className={`language-${language}`}>{code}</code>
+      <code ref={ref} className={`language-${language}`}>{code}</code>
     </pre>
   );
 }
